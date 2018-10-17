2327551f09c88aa5f5529085897366459a259ae7
Chet Corcos <ccorcos@gmail.com>
2018-10-17 10:18:15 -0700

    Simplest browsing

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -7,9 +7,45 @@ import * as ReactDOM from "react-dom"
 
 const root = document.getElementById("root")
 
-class App extends React.Component {
+interface AppProps {}
+
+interface AppState {
+	inputValue: string
+	currentUrl: string
+}
+
+class App extends React.PureComponent<AppProps, AppState> {
+	state = {
+		inputValue: "",
+		currentUrl: "http://www.chetcorcos.com",
+	}
+
+	private handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const target = event.target as HTMLInputElement
+		this.setState({ inputValue: target.value })
+	}
+
+	private handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+		if (event.key === "Enter") {
+			this.setState({ currentUrl: this.state.inputValue })
+		}
+	}
+
 	render() {
-		return <div>hey there</div>
+		return (
+			<div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
+				<div style={{ padding: 8 }}>
+					<input
+						type="text"
+						value={this.state.inputValue}
+						onKeyPress={this.handleKeyPress}
+						onChange={this.handleInputChange}
+					/>
+				</div>
+
+				<webview style={{ flex: 1 }} src={this.state.currentUrl} />
+			</div>
+		)
 	}
 }
 
